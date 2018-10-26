233ef97eb9c416de8afad585b902daa675e2cc13
Eric Rybarczyk <EricRybarczyk@gmail.com>
2018-10-25 20:52:16 -0400

    Resolved issue with App Theme and Toolbar configuration

--- app/src/main/java/me/ericrybarczyk/roadtrippy/triplist/TripListActivity.java
+++ app/src/main/java/me/ericrybarczyk/roadtrippy/triplist/TripListActivity.java
@@ -2,13 +2,37 @@ package me.ericrybarczyk.roadtrippy.triplist;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import me.ericrybarczyk.roadtrippy.R;
 
 public class TripListActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) protected Toolbar toolbar;
+    @BindView(R.id.drawer_layout) protected DrawerLayout drawer;
+    @BindView(R.id.nav_view) protected NavigationView navigationView;
+    @BindView(R.id.content_container) protected FrameLayout contentFrameLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_triplist);
+        ButterKnife.bind(this);
 
+        // configure toolbar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
