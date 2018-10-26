ec7efabe82e6d2857e8a63c591244b75a921db40
Eric Rybarczyk <EricRybarczyk@gmail.com>
2018-10-25 20:06:08 -0400

    Initial setup for reworking the app

---
+++ app/src/main/java/me/ericrybarczyk/roadtrippy/triplist/TripListActivity.java
@@ -0,0 +1,14 @@
+package me.ericrybarczyk.roadtrippy.triplist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class TripListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
