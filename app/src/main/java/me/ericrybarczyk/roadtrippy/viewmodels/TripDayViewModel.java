f29e49584a821cef188a435e000480209ca513ae
Eric Rybarczyk <EricRybarczyk@gmail.com>
2018-10-27 13:55:24 -0400

    Set up View Model classes

---
+++ app/src/main/java/me/ericrybarczyk/roadtrippy/viewmodels/TripDayViewModel.java
@@ -0,0 +1,187 @@
+package me.ericrybarczyk.roadtrippy.viewmodels;
+
+import android.arch.lifecycle.ViewModel;
+
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+
+import ericrybarczyk.me.roadtrippy.dto.TripDay;
+import ericrybarczyk.me.roadtrippy.dto.TripLocation;
+import ericrybarczyk.me.roadtrippy.persistence.PersistenceFormats;
+
+public class TripDayViewModel extends ViewModel {
+
+    private String tripDayId;
+    private String tripDayNodeKey; // key to TripDay object in Firebase (the pushKey for the TripDay)
+    private String tripId; // key to Trip object that contains this TripDay
+    private String tripNodeKey; // key to Trip object in Firebase (the pushKey for the Trip) that contains this TripDay
+    private int dayNumber;
+    private boolean isDrivingDay;
+    private boolean isHighlight;
+    private LocalDate tripDayDate;
+    private String primaryDescription;
+    private String userNotes;
+    private ArrayList<TripLocationViewModel> destinations;
+    private boolean isDefaultText;
+
+    public TripDayViewModel() {
+        destinations = new ArrayList<>();
+    }
+
+    public static TripDayViewModel from(TripDay tripDay) {
+        TripDayViewModel viewModel = new TripDayViewModel();
+
+        viewModel.setTripDayId(tripDay.getTripDayId());
+        viewModel.setTripDayNodeKey(tripDay.getTripDayNodeKey());
+        viewModel.setTripId(tripDay.getTripId());
+        viewModel.setTripNodeKey(tripDay.getTripNodeKey());
+        viewModel.setDayNumber(tripDay.getDayNumber());
+        viewModel.setIsDrivingDay(tripDay.getIsDrivingDay());
+        viewModel.setIsHighlight(tripDay.getIsHighlight());
+        viewModel.setTripDayDate(LocalDate.parse(tripDay.getTripDayDate()));
+        viewModel.setPrimaryDescription(tripDay.getPrimaryDescription());
+        viewModel.setUserNotes(tripDay.getUserNotes());
+        viewModel.setIsDefaultText(tripDay.getIsDefaultText());
+        for (TripLocation loc : tripDay.getDestinations()) {
+            viewModel.getDestinations().add(TripLocationViewModel.from(loc));
+        }
+
+        return viewModel;
+    }
+
+    public void updateFrom(TripDay tripDay) {
+        this.setTripDayId(tripDay.getTripDayId());
+        this.setTripDayNodeKey(tripDay.getTripDayNodeKey());
+        this.setTripId(tripDay.getTripId());
+        this.setTripNodeKey(tripDay.getTripNodeKey());
+        this.setDayNumber(tripDay.getDayNumber());
+        this.setIsDrivingDay(tripDay.getIsDrivingDay());
+        this.setIsHighlight(tripDay.getIsHighlight());
+        this.setTripDayDate(LocalDate.parse(tripDay.getTripDayDate()));
+        this.setPrimaryDescription(tripDay.getPrimaryDescription());
+        this.setUserNotes(tripDay.getUserNotes());
+        this.setIsDefaultText(tripDay.getIsDefaultText());
+        this.setDestinations(new ArrayList<>());
+        for (TripLocation loc : tripDay.getDestinations()) {
+            this.getDestinations().add(TripLocationViewModel.from(loc));
+        }
+    }
+
+    public TripDay asTripDay() {
+        TripDay tripDay = new TripDay();
+        tripDay.setTripDayId(this.getTripDayId());
+        tripDay.setTripDayNodeKey(this.getTripDayNodeKey());
+        tripDay.setTripNodeKey(this.getTripNodeKey());
+        tripDay.setTripId(this.getTripId());
+        tripDay.setDayNumber(this.getDayNumber());
+        tripDay.setIsDrivingDay(this.getIsDrivingDay());
+        tripDay.setIsHighlight(this.getIsHighlight());
+        tripDay.setTripDayDate(PersistenceFormats.toDateString(this.getTripDayDate()));
+        tripDay.setPrimaryDescription(this.getPrimaryDescription());
+        tripDay.setUserNotes(this.getUserNotes());
+        tripDay.setIsDefaultText(this.getIsDefaultText());
+        for (TripLocationViewModel loc : this.getDestinations()) {
+            tripDay.getDestinations().add(loc.asTripLocation());
+        }
+
+        return tripDay;
+    }
+
+    public String getTripDayId() {
+        return tripDayId;
+    }
+
+    public void setTripDayId(String tripDayId) {
+        this.tripDayId = tripDayId;
+    }
+
+    public String getTripDayNodeKey() {
+        return tripDayNodeKey;
+    }
+
+    public void setTripDayNodeKey(String tripDayNodeKey) {
+        this.tripDayNodeKey = tripDayNodeKey;
+    }
+
+    public String getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(String tripId) {
+        this.tripId = tripId;
+    }
+
+    public String getTripNodeKey() {
+        return tripNodeKey;
+    }
+
+    public void setTripNodeKey(String tripNodeKey) {
+        this.tripNodeKey = tripNodeKey;
+    }
+
+    public int getDayNumber() {
+        return dayNumber;
+    }
+
+    public void setDayNumber(int dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+
+    public boolean getIsDrivingDay() {
+        return isDrivingDay;
+    }
+
+    public void setIsDrivingDay(boolean drivingDay) {
+        isDrivingDay = drivingDay;
+    }
+
+    public boolean getIsHighlight() {
+        return isHighlight;
+    }
+
+    public void setIsHighlight(boolean highlight) {
+        isHighlight = highlight;
+    }
+
+    public LocalDate getTripDayDate() {
+        return tripDayDate;
+    }
+
+    public void setTripDayDate(LocalDate tripDayDate) {
+        this.tripDayDate = tripDayDate;
+    }
+
+    public String getPrimaryDescription() {
+        return primaryDescription;
+    }
+
+    public void setPrimaryDescription(String primaryDescription) {
+        this.primaryDescription = primaryDescription;
+    }
+
+    public String getUserNotes() {
+        return userNotes;
+    }
+
+    public void setUserNotes(String userNotes) {
+        this.userNotes = userNotes;
+    }
+
+    public ArrayList<TripLocationViewModel> getDestinations() {
+        return destinations;
+    }
+
+    public void setDestinations(ArrayList<TripLocationViewModel> destinations) {
+        this.destinations = destinations;
+    }
+
+    public boolean getIsDefaultText() {
+        return isDefaultText;
+    }
+
+    public void setIsDefaultText(boolean defaultText) {
+        isDefaultText = defaultText;
+    }
+}
---
+++ app/src/main/java/me/ericrybarczyk/roadtrippy/viewmodels/TripViewModel.java
@@ -0,0 +1,243 @@
+package me.ericrybarczyk.roadtrippy.viewmodels;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.threeten.bp.Duration;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.format.TextStyle;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.UUID;
+
+import ericrybarczyk.me.roadtrippy.dto.Trip;
+
+
+public class TripViewModel extends ViewModel {
+    private String tripId;
+    private String description;
+    private LocalDate departureDate;
+    private LocalDate returnDate;
+    private LatLng originLatLng;
+    private String originDescription;
+    private LatLng destinationLatLng;
+    private String destinationDescription;
+    private int durationMinutes;
+    private boolean includeReturn;
+    private boolean isEdited; // to help UI know if values are selected by user or just new instance defaults
+
+    public TripViewModel() {
+        init();
+    }
+
+    private void init() {
+        tripId = UUID.randomUUID().toString();
+        description = "";
+        originDescription = "";
+        destinationDescription = "";
+        departureDate = LocalDate.now();
+        returnDate = LocalDate.now().plusDays(1);
+        includeReturn = true;
+        durationMinutes = 0;
+        if (isEdited) {
+            originLatLng = null;
+            destinationLatLng = null;
+            isEdited = false;
+        }
+    }
+
+    public static TripViewModel from(Trip trip) {
+        TripViewModel viewModel = new TripViewModel();
+
+        viewModel.setTripId(trip.getTripId());
+        viewModel.setDescription(trip.getDescription());
+        viewModel.setDepartureDate(LocalDate.parse(trip.getDepartureDate()));
+        viewModel.setReturnDate(LocalDate.parse(trip.getReturnDate()));
+        viewModel.setOriginLatLng(new LatLng(trip.getOriginLatitude(), trip.getOriginLongitude()));
+        viewModel.setOriginDescription(trip.getOriginDescription());
+        viewModel.setDestinationLatLng(new LatLng(trip.getDestinationLatitude(), trip.getDestinationLongitude()));
+        viewModel.setDestinationDescription(trip.getDestinationDescription());
+        viewModel.setIncludeReturn(trip.getIncludeReturn());
+        viewModel.setDurationMinutes(trip.getDurationMinutes());
+
+        return viewModel;
+    }
+
+    public void updateFrom(Trip trip) {
+        this.setTripId(trip.getTripId());
+        this.setDescription(trip.getDescription());
+        this.setDepartureDate(LocalDate.parse(trip.getDepartureDate()));
+        this.setReturnDate(LocalDate.parse(trip.getReturnDate()));
+        this.setOriginLatLng(new LatLng(trip.getOriginLatitude(), trip.getOriginLongitude()));
+        this.setDestinationLatLng(new LatLng(trip.getDestinationLatitude(), trip.getDestinationLongitude()));
+        this.setOriginDescription(trip.getOriginDescription());
+        this.setDestinationDescription(trip.getDestinationDescription());
+        this.setIncludeReturn(trip.getIncludeReturn());
+        this.setDurationMinutes(trip.getDurationMinutes());
+    }
+
+    public void reset() {
+        init();
+    }
+
+    public String getOriginDestinationSummaryText(String joinWord) {
+
+        return originDescription + " "
+                + joinWord + " "
+                + destinationDescription;
+    }
+
+    public String getDateRangeSummaryText(String joinWord) {
+        if (departureDate.getMonthValue() == returnDate.getMonthValue()) {
+            return departureDate.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()) + " "
+                    + String.valueOf(departureDate.getDayOfMonth()) + " "
+                    + joinWord + " "
+                    + String.valueOf(returnDate.getDayOfMonth());
+        } else {
+            return departureDate.getMonth().getDisplayName(TextStyle.SHORT, Locale.getDefault()) + " "
+                    + String.valueOf(departureDate.getDayOfMonth()) + " "
+                    + joinWord + " "
+                    + returnDate.getMonth().getDisplayName(TextStyle.SHORT, Locale.getDefault()) + " "
+                    + String.valueOf(returnDate.getDayOfMonth());
+        }
+    }
+
+    public String getDurationDescription(String hours, String minutes, String h, String m, String unknown) {
+        if (durationMinutes == 0) {
+            return unknown;
+        } else if (durationMinutes < 60) {
+            return String.valueOf(durationMinutes) + " " + minutes;
+        } else if (durationMinutes % 60 == 0) {
+            int numHours = durationMinutes / 60;
+            return String.valueOf(numHours) + " " + hours;
+        } else {
+            int numHours = durationMinutes / 60;
+            int numMinutes = durationMinutes - (numHours * 60);
+            return String.valueOf(numHours) + h + " " + String.valueOf(numMinutes) + m;
+        }
+
+    }
+
+    public int getDaysUntilDeparture() {
+
+        LocalDateTime now = LocalDateTime.of(
+                LocalDateTime.now().getYear(),
+                LocalDateTime.now().getMonthValue(),
+                LocalDateTime.now().getDayOfMonth(),
+                0, 0, 0);
+
+        LocalDateTime departure = LocalDateTime.of(
+                this.getDepartureDate().getYear(),
+                this.getDepartureDate().getMonthValue(),
+                this.getDepartureDate().getDayOfMonth(),
+                0,0,0);
+
+        Duration duration = Duration.between(now, departure);
+        int daysToGo = (int)duration.toDays(); // add one to make it inclusive of last day
+        if (daysToGo < 0) {
+            return 0;
+        }
+        return daysToGo;
+    }
+
+    public String getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(String tripId) {
+        this.tripId = tripId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        isEdited = true;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+        isEdited = true;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+        isEdited = true;
+    }
+
+    public LatLng getOriginLatLng() {
+        return originLatLng;
+    }
+
+    public void setOriginLatLng(LatLng originLatLng) {
+        this.originLatLng = originLatLng;
+        isEdited = true;
+    }
+
+    public LatLng getDestinationLatLng() {
+        return destinationLatLng;
+    }
+
+    public void setDestinationLatLng(LatLng destinationLatLng) {
+        this.destinationLatLng = destinationLatLng;
+        isEdited = true;
+    }
+
+    public String getOriginDescription() {
+        if (originDescription == null) {
+            return "";
+        }
+        return originDescription;
+    }
+
+    public void setOriginDescription(String originDescription) {
+        this.originDescription = originDescription;
+        isEdited = true;
+    }
+
+    public String getDestinationDescription() {
+        if (destinationDescription == null) {
+            return "";
+        }
+        return destinationDescription;
+    }
+
+    public void setDestinationDescription(String destinationDescription) {
+        this.destinationDescription = destinationDescription;
+        isEdited = true;
+    }
+
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    public void setDurationMinutes(int durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+
+    public boolean isIncludeReturn() {
+        return includeReturn;
+    }
+
+    public void setIncludeReturn(boolean includeReturn) {
+        this.includeReturn = includeReturn;
+        isEdited = true;
+    }
+
+    public boolean isEdited() {
+        return isEdited;
+    }
+}
